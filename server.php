7798378144df4e76c889da470b3fc4893045d2f3
jasonwubz <jia.cheng.wu@gmail.com>
2019-08-07 21:27:41 -0600

    adds quit command

--- server.php
+++ server.php
@@ -70,9 +70,9 @@ function HandleServerSocket(&$server_socket)
 
     socket_getpeername($new_client, $client_ip, $client_port);
 
-    echo date('H:i:s') . " New client connected [{$client_ip}:{$client_port}]" . PHP_EOL;
+    echo date('H:i:s') . " New client connected \33[44m[{$client_ip}:{$client_port}]\33[0m" . PHP_EOL;
     
-    //server announces to everyone
+    // adds announcement to queue so that server will send to everyone
     $new_key = (integer) count($broadcast_queue) + 1;
     $broadcast_queue = [
         $new_key => [
@@ -98,6 +98,7 @@ function HandleClientSocket(&$read_socket)
         return;
     }
     
+    // if there is some buffer, we want to at least let the server know
     if (strlen($buffer) > 0) {
         if (strlen($buffer) == 1 && $buffer == "\n") {
             return;
@@ -114,18 +115,22 @@ function HandleClientSocket(&$read_socket)
     }
 
     if ($peek_result && (strpos($buffer, "\n") === false && strlen($buffer) < 1024)) {
-        //not ready to read
+        // not ready to read
         return;
     }
 
     // read until newline (due to PHP_NORMAL_READ that has higher preference) or 1024 bytes
     $data = @socket_read($read_socket, 1024, PHP_NORMAL_READ);
 
+    $key = array_search($read_socket, $clients);
+    $client_ip = '';
+    $client_port = 0;
+    socket_getpeername($clients[$key], $client_ip, $client_port);
+    
     // if client disconnected, remove client from array
     if ($data === false) {
-        $key = array_search($read_socket, $clients);
         unset($clients[$key]);
-        echo date('H:i:s') . " client disconnected [{$key}]" . PHP_EOL;
+        echo date('H:i:s') . " client disconnected [{$client_ip}:{$client_port}]" . PHP_EOL;
         return;
     }
 
@@ -133,9 +138,31 @@ function HandleClientSocket(&$read_socket)
 
     // check if there is any data after trimming off the spaces
     if (!empty($data)) {
-        $client_ip = '';
-        $client_port = 0;
-        socket_getpeername($read_socket, $client_ip, $client_port);
+
+        if (strcasecmp($data, "quit") === 0) {
+            //use sends quit command, sad to see them go
+            socket_write(
+                $read_socket, 
+                PHP_EOL .
+                "\33[44mconnection terminated, we are sad to see you go...\33[0m" . 
+                PHP_EOL
+            );
+            
+            // disconnect and remove from clients then send broadcast
+            socket_close($read_socket);
+            unset($clients[$key]);
+
+            $new_key = (integer) count($broadcast_queue) + 1;
+            $broadcast_queue = [
+                $new_key => [
+                    "message" => "{$client_port} has left the chat :(",
+                    "who" => "SERVER",
+                    "who_socket" => $read_socket
+                ]
+            ] + $broadcast_queue;
+            
+            return;
+        }        
         
         echo PHP_EOL . date('H:i:s') . " [{$client_ip}:{$client_port}] says: {$data}" . PHP_EOL;
 
@@ -165,7 +192,7 @@ function BroadcastMessage(&$write_socket, $message, $who_said, &$who_socket) {
 }
 
 if (function_exists('socket_create') === false) {
-    exit('fatal: socket functions are not compiled, cannot continue' .  PHP_EOL);
+    exit("\33[0mfatal: socket functions are not compiled, cannot continue\33[0m" . PHP_EOL);
 }
 
 if (function_exists('xdebug_is_enabled') && xdebug_is_enabled()) {
@@ -178,7 +205,7 @@ $server_socket = @socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
 if ($server_socket === false) {
     $errorcode = socket_last_error();
     $errormsg = socket_strerror($errorcode);
-    exit("Unable to create socket: [$errorcode] $errormsg");
+    exit("\33[41mfatal: unable to create socket: [$errorcode] {$errormsg}\33[0m" . PHP_EOL);
 }
 
 // set the option to reuse the port
@@ -186,7 +213,7 @@ socket_set_option($server_socket, SOL_SOCKET, SO_REUSEADDR, 1);
 
 // binds the socket address 0 on port $server_port
 if (!socket_bind($server_socket, 0, $server_port)) {
-    exit('Unable to bind socket');
+    exit("\33[41mfatal: unable to bind socket\33[0m" . PHP_EOL);
 }
 
 // start listen for connections
@@ -274,7 +301,7 @@ while (true) {
 
         if (is_array($except_sockets) && count($except_sockets)) {
             foreach ($except_sockets as $e_key => &$e_socket) {
-                echo 'e socket' . PHP_EOL;
+                //TODO: handle exceptions
             }
         }
 
